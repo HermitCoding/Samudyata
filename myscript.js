20117e28ca753604a6adb6c912899f3215f9ceea
HermitCoding <79991243+HermitCoding@users.noreply.github.com>
2022-12-26 16:30:29 +0000

    26-12 commit

--- myscript.js
+++ myscript.js
@@ -1,6 +1,10 @@
 const hero = document.getElementById('hero-img');
+const para = document.getElementById('paraimg');
+
 window.addEventListener('scroll', zoomHero);
-function zoomHero(){
-  hero.style.backgroundSize = 100 +window.pageYOffset/12+'%';
+function zoomHero() {
+  let offset = window.pageYOffset - para.offsetHeight;
+  hero.style.backgroundSize = 100 + window.pageYOffset / 12 + '%';
+  para.style.backgroundPositionY = offset * 0.75 + "px";
 }
 
