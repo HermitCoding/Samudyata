a2434830a9f7ba7fa2fe9cf3869d9711529dc142
HermitCoding <79991243+HermitCoding@users.noreply.github.com>
2022-12-28 09:27:56 +0000

    paralax fixed

--- myscript.js
+++ myscript.js
@@ -3,7 +3,7 @@ const para = document.getElementById('paraimg');
 
 window.addEventListener('scroll', zoomHero);
 function zoomHero() {
-  let offset = window.pageYOffset - para.offsetHeight;
+  let offset = window.pageYOffset - para.offsetTop;
   hero.style.backgroundSize = 100 + window.pageYOffset / 12 + '%';
   para.style.backgroundPositionY = offset * 0.7 + "px";
 }
